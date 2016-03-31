9c7cdea3186193f6062f1b6e53c7d093e2cae32e
Adam Perron <me@adamperron.com>
2016-03-30 22:00:40 -0400

    blessed

--- pages/viewproperty.php
+++ pages/viewproperty.php
@@ -18,33 +18,116 @@ include "pages/banner.php";
 
 ?>
 
+<div class="huge-padding"></div>
+
 <div class="container">
 
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4 info-property-pic-div"><img class="info-property-pic" src="qbnb_website/images/property_1.jpg"></div>
-		<div class="col-md-4"><div class="info-property-price">$<?php echo $property->getPrice(); ?>.00</div></div>
-	</div>
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <img class="info-property-pic" width="100%" src="images/property_1.jpg">
+      <div class="info-property-price"><?php echo $property->getPrice(); ?></div>
+    </div>
+  </div>
 
-	<?php if (User::current()->isAdmin() ||  $property->getSupplierID() === User::current()->getID()) { ?>
-		<a class="pencil-prop-icon" style="position: static" href="./?property=<?php echo $property->getID(); ?>&edit"><i class="fa fa-pencil"></i></a>
-		<a class="remove-prop-icon" style="position: static" href="./?property=<?php echo $property->getID(); ?>&delete"><i class="fa fa-times"></i></a>
+  <br>
 
-	<?php } ?>
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="info-property-title form-group"><?php echo $property->getName(); ?></div>
+    </div>
+  </div>
 
-	<div class="row">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="info-property-subtitle form-group"><?php echo $property->getAddress(); ?></div>
+    </div>
+  </div>
 
-		<div class="center-text">
-			<div class="info-property-title"><?php echo $property->getName(); ?></div>
-			<div class="info-property-subtitle"><?php echo $property->getAddress(); ?></div>
-			<div class="info-property-type">Room type: <span><?php echo $property->getPropertyTypeName(); ?></span></div>
+    <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <p class="property-description form-group"><?php echo $property->getDescription(); ?></p>
+    </div>
+  </div>
 
-			<p class="property-description">Description:<br><?php echo $property->getDescription(); ?></p>
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="info-property-type">
+        <table class="table table-striped">
+          <tbody>
+            <tr>
+              <td><strong>Property District</strong></td>
+              <td><?php echo $property->getDistrictID(); ?></td>
+            </tr>
+            <tr>
+              <td><strong>Property Type</strong></td>
+              <td><?php echo $property->getPropertyTypeID(); ?></td>
+            </tr>
+            <tr>
+              <td><strong>Max Guests</strong></td>
+              <td><?php echo $property->getNumGuests(); ?></td>
+            </tr>
+            <tr>
+              <td><strong>Number of Rooms</strong></td>
+              <td><?php echo $property->getNumRooms(); ?></td>
+            </tr>
+            <tr>
+              <td><strong>Number of Bathrooms</strong></td>
+              <td><?php echo $property->getNumBathrooms(); ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have air conditioning</strong></td>
+              <td><?php echo ($features["has_air_conditioning"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have cable tv</strong></td>
+              <td><?php echo ($features["has_cable_tv"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have laundry machines</strong></td>
+              <td><?php echo ($features["has_laundry_machines"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have parking</strong></td>
+              <td><?php echo ($features["has_parking"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have a gym</strong></td>
+              <td><?php echo ($features["has_gym"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have internet</strong></td>
+              <td><?php echo ($features["has_internet"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I allow pets</strong></td>
+              <td><?php echo ($features["pets_allowed"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have wheelchair access</strong></td>
+              <td><?php echo ($features["has_wheelchair_access"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have a pool</strong></td>
+              <td><?php echo ($features["has_pool"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have access to transport</strong></td>
+              <td><?php echo ($features["has_transport_access"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
+            <tr>
+              <td><strong>I have a private bathroom</strong></td>
+              <td><?php echo ($features["has_private_bathroom"]) ? 'Yes' : 'No'; ?></td>
+            </tr>
 
-		</div>
-
-	</div>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 
 </div>
+
+<div class="huge-padding"></div>
 
 <?php
 
